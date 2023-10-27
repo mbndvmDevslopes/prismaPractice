b42503709d9f4c996c03677655f509fb0ebcd4a3
m <mbddvm@gmail.com>
2023-10-27 09:32:20 -0500

    P3, p4

--- problems/p4.ts
+++ problems/p4.ts
@@ -1,4 +1,15 @@
 import { prisma } from "./prisma";
 
 // get All Pg-13 movies, ordered by release year descending
-export const getAllPG13Movies = () => {};
+export const getAllPG13Movies = () => {
+  const pg13Movies = prisma.movie.findMany({
+    where: {
+      parentalRating: 'PG-13',
+    },
+    orderBy: {
+      releaseYear: 'desc',
+    },
+  });
+  console.log(pg13Movies);
+  return pg13Movies;
+};
