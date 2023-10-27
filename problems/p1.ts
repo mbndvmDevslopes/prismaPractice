ef3ce63678fddfda67a0b8d75d3e33a5c93e43b9
m <mbddvm@gmail.com>
2023-10-27 08:59:41 -0500

    Add scripts for tests and finish p1, p2

--- problems/p1.ts
+++ problems/p1.ts
@@ -1,5 +1,12 @@
-import { prisma } from "./prisma";
+import { prisma } from './prisma';
 
 // Hint: look up "orderBy"
 // get an array of all users
-export const getAllUsers = () => {};
+export const getAllUsers = async () => {
+  const users = await prisma.user.findMany({
+    orderBy: {
+      username: 'asc',
+    },
+  });
+  return users;
+};
